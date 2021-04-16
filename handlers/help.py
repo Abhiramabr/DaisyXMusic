bff5be3a2ca8a629a55e882ab382df88942d103a
Abhiramabr <64951627+Abhiramabr@users.noreply.github.com>
2021-04-16 16:47:52 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,20 +1,23 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-PM_HELP_TEXT = """Commands 🛠
-For all in group
+PM_HELP_TEXT = """ ADD @musicx3stream and me to your Group!!
 
-    /play - reply to youtube url or song file to play song
-    /play <song name> - play song you requested
-    /song <song name> - download songs you want quickly
-    /search <query> - search videos on youtube with details
+Commands 🛠
 
-Admins only
+For everyone in the group
 
-    /pause - pause song play
-    /resume - resume song play
-    /skip - play next song
-    /end - stop music play
+/play - reply to youtube url or song file to play song
+/play <song name> - play song you requested
+/song <song name> - download songs you want quickly
+/search <query> - search videos on youtube with details
+
+Admins only commands
+
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
 """ 
 
 @Client.on_message(filters.command(["help"]))
