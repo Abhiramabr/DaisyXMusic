b8e584dcaa6bf7746056311c56941d3450646e31
Abhiramabr <64951627+Abhiramabr@users.noreply.github.com>
2021-04-16 15:27:20 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,42 +1,50 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import BOT_NAME as bn
 
-
-
-
-@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
-async def start(_, message: Message):
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hey {message.from_user.first_name}!</b>
 I can play music in your Telegram's voice chat!
 Use the buttons below to know more about how to use me.""",
         reply_markup=InlineKeyboardMarkup(
-            [ 
-                [
-                    InlineKeyboardButton(
-                         "📞 Contact", url="https://t.me/T_r_oy"
-                    ),
-                 
-                    InlineKeyboardButton(
-                        "🛸 Channel", url="https://t.me/Wandabetaxbot"
-                    )]
-            ]
-        ),
-     disable_web_page_preview=True
-    )
-
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
-async def gstart(_, message: Message):
-      await message.reply_text("""**🔴 Music player is online**""",
-      reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
+                        "📞 Contact", url="https://t.me/T_r_oy"
+                    ),
+                    InlineKeyboardButton(
                         "🛸 Channel", url="https://t.me/Wandabetaxbot"
+                    )
                 ]
             ]
         )
-   )
+    )
 
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
