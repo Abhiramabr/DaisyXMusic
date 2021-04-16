798a47fcbb86d423290c8396b08eb0508978b95d
Abhiramabr <64951627+Abhiramabr@users.noreply.github.com>
2021-04-16 15:00:40 +0530

    Update and rename start to start.py

---
+++ handlers/start.py
@@ -0,0 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""<b>Hey {message.from_user.first_name}!</b>
+I can play music in your Telegram's voice chat!
+Use the buttons below to know more about how to use me.""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                         "📞 Contact", url="https://t.me/T_r_oy"
+                    ),
+                 
+                    InlineKeyboardButton(
+                        "🛸 Channel", url="https://t.me/Wandabetaxbot"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**🔴 Music player is online**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🛸 Channel", url="https://t.me/Wandabetaxbot"
+                ]
+            ]
+        )
+   )
+
